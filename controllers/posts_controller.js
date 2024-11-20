1d11ea9a3683cdbcd7b093f6f4b0d8f786beb314
omervavd3 <omervavd3@gmail.com>
2024-11-20 18:15:13 +0200

    update

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,6 +1,12 @@
 const getAllPosts = async (req, res) => {
   try {
-    res.status(200).send("All posts");
+    const filter = req.query.sender;
+    if (filter) {
+      res.status(200).send("Posts by owner: " + filter);
+      return;
+    } else {
+      res.status(200).send("All posts");
+    }
   } catch (error) {
     res.status(400).send(error.message);
   }
@@ -24,8 +30,29 @@ const getPostById = async (req, res) => {
   }
 };
 
+const getPostBySender = async (req, res) => {
+  try {
+    const sender = req.params.sender;
+    res.status(200).send("Sender: " + sender);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+}
+
+const updatePost = async (req, res) => {
+  try {
+    const postId = req.params.postId;
+    const {updatedPost} = req.body;
+    res.status(200).send("Post ID: " + postId + " updated ");
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
 module.exports = {
   getAllPosts,
   createPost,
   getPostById,
+  getPostBySender,
+  updatePost,
 };
--- routes/posts_routes.js
+++ routes/posts_routes.js
@@ -6,6 +6,8 @@ const postsController = require("../controllers/posts_controller");
 postsRouter
   .get("/", postsController.getAllPosts)
   .post("/", postsController.createPost)
-  .get("/:postId", postsController.getPostById);
+  .get("/:postId", postsController.getPostById)
+  .get("/:sender", postsController.getPostBySender)
+  .put("/:postId", postsController.updatePost);
 
 module.exports = postsRouter;
